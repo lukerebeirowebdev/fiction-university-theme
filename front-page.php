9d6112346f1d88e0b7ddb8d2770cc0112761ccfe
Luke Rebiro <68423198+lukerebeirowebdev@users.noreply.github.com>
2021-03-15 10:55:44 +0000

    updated front-end

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large"></h1>
+        <h1 class="headline headline--large">Welcome!!!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
         <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
@@ -63,7 +63,7 @@
                 <?php } wp_reset_postdata();
             ?>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+            <p class="t-center no-margin"><a href="<?php echo site_url('/blog/') ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
     </div>
 </div>
